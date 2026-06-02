d94afe6c030eb73262fcdff3b9a29399f45ee913
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-06-02 14:52:52 +0200

    Bump Kotlin Multiplatform to 2.4.0-RC2 and Kotlin Wrappers to 2026.6.0.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("kotlinWrappers") {
-            val wrappersVersion = "2026.5.3"
+            val wrappersVersion = "2026.6.0"
             from("org.jetbrains.kotlin-wrappers:kotlin-wrappers-catalog:$wrappersVersion")
         }
     }
